db6e685a373e17e89eebc26db757482256d8d8ed
jmoran40 <jmoran40@charlottesvilleschools.org>
2023-08-29 15:46:53 -0400

    edits

--- neopixel_rainbowio_simpletest.py
+++ neopixel_rainbowio_simpletest.py
@@ -7,8 +7,8 @@ from rainbowio import colorwheel
 import neopixel
 
 NUMPIXELS = 1  # Update this to match the number of LEDs.
-SPEED = 1  # Increase to slow down the rainbow. Decrease to speed it up.
-BRIGHTNESS = 1  # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
+SPEED = 0.1  # Increase to slow down the rainbow. Decrease to speed it up.
+BRIGHTNESS = 0.2  # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is max.
 PIN = board.NEOPIXEL  # This is the default pin on the 5x5 NeoPixel Grid BFF.
 
 pixels = neopixel.NeoPixel(PIN, NUMPIXELS, brightness=BRIGHTNESS, auto_write=False)
