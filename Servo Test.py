f68817ef83d25c04878c4db3b8b112c90a913682
jmoran40 <jmoran40@charlottesvilleschools.org>
2023-08-31 15:53:13 -0400

    burns

--- Servo Test.py
+++ Servo Test.py
@@ -11,9 +11,9 @@ pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(20, 120, 5):  # 0 - 180 degrees, 5 degrees at a time.
+    for angle in range(50, 100, 5):  # 0 - 180 degrees, 5 degrees at a time.
         my_servo.angle = angle
-        time.sleep(0.05)
-    for angle in range(120, 20, -5): # 180 - 0 degrees, 5 degrees at a time.
+        time.sleep(0.1)
+    for angle in range(100, 50, -5): # 180 - 0 degrees, 5 degrees at a time.
         my_servo.angle = angle
-        time.sleep(0.05)
+        time.sleep(0.1)
