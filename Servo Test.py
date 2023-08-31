275c5e87b9e7b454d00f67bd4a890280a5073529
jmoran40 <jmoran40@charlottesvilleschools.org>
2023-08-31 15:35:07 -0400

    added servo library

--- Servo Test.py
+++ Servo Test.py
@@ -2,7 +2,7 @@
 import time
 import board
 import pwmio
-from adafruit_motor import servo
+import servo
 
 # create a PWMOut object on Pin A2.
 pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
