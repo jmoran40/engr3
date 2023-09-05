83586b3d6d901ddcb6851001847d1f2949bc08e4
jmoran40 <jmoran40@charlottesvilleschools.org>
2023-09-05 15:55:34 -0400

    got started on button test

---
+++ Button Test.py
@@ -0,0 +1,4 @@
+import board
+import digitalio
+buttonRED = digitalio.DigitalInOut(board.A4)
+buttonBLUE = digitalio.DigitalInOut(board.A5)
--- Servo Test.py
+++ Servo Test.py
@@ -2,7 +2,7 @@
 import time
 import board
 import pwmio
-import servo
+from adafruit_servokit import servo
 
 # create a PWMOut object on Pin A2.
 pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
@@ -11,9 +11,9 @@ pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(50, 100, 5):  # 0 - 180 degrees, 5 degrees at a time.
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
         my_servo.angle = angle
         time.sleep(0.1)
-    for angle in range(100, 50, -5): # 180 - 0 degrees, 5 degrees at a time.
+    for angle in range(0, 180, -5): # 180 - 0 degrees, 5 degrees at a time.
         my_servo.angle = angle
         time.sleep(0.1)
