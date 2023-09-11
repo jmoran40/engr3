8996e8df1c712943749a31cb281cf35c5bc00a83
jmoran40 <jmoran40@charlottesvilleschools.org>
2023-09-11 15:41:39 -0400

    It works

---
+++ Trials and Tests/ButtonFunctionTest.py
@@ -0,0 +1,22 @@
+import time
+import board
+import pwmio
+from adafruit_motor import servo
+from digitalio import DigitalInOut, Direction, Pull
+
+pwn = pwmio.PWMOut(board.D5, duty_cycle=0, frequency=50)
+
+buttonRED = DigitalInOut(board.A4)
+buttonRED.direction = Direction.INPUT
+buttonRED.pull = Pull.DOWN
+
+buttonBLUE = DigitalInOut(board.A5)
+buttonBLUE.direction = Direction.INPUT
+buttonBLUE.pull = Pull.DOWN
+
+servo = servo.Servo(pwn)
+
+while True:
+    print(buttonRED)
+    print(buttonBLUE)
+    time.sleep(0.1)
