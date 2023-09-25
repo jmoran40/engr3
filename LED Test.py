89bc31882d9497f9477e79f4981fe44f6dedb3b6
jmoran40 <jmoran40@charlottesvilleschools.org>
2023-09-25 15:24:52 -0400

    rrrrrrr

--- LED Test.py
+++ LED Test.py
@@ -1,6 +1,5 @@
 import time
 import board
-import adafruit_hcsr04
 import neopixel
 
 NUMPIXELS = 1  # Update this to match the number of LEDs.
@@ -9,12 +8,17 @@ BRIGHTNESS = 0.2  # A number between 0.0 and 1.0, where 0.0 is off, and 1.0 is m
 PIN = board.NEOPIXEL  # This is the default pin on the 5x5 NeoPixel Grid BFF.
 
 pixels = neopixel.NeoPixel(PIN, NUMPIXELS, brightness=BRIGHTNESS, auto_write=False)
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 while True:
-    try:
-        print((sonar.distance,))
-    except RuntimeError:
-        print("Retrying!")
-    time.sleep(0.1)
-    
+    pixels.fill(RED)
+    pixels.show()
+    time.sleep(2)
+    pixels.fill(GREEN)
+    pixels.show()
+    time.sleep(2)
+    pixels.fill(BLUE)
+    pixels.show()
+    time.sleep(2)
