c0d3024cc2be5039e1c6ba9278b5df799edd9bf6
jmoran40 <jmoran40@charlottesvilleschools.org>
2024-01-18 15:51:52 -0500

    save

--- Trials and Tests/Rotary Encoder & LCD.py
+++ Trials and Tests/Rotary Encoder & LCD.py
@@ -17,6 +17,11 @@ button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 button_state = None
 
+# Menu/List Setup
+menu = ["stop", "caution", "go" ]
+last_index = None
+menu_index = 0
+
 while True:
     # This code registers when the button is pressed
     if not button.value and button_state is None:
@@ -24,3 +29,8 @@ while True:
     if button.value and button_state == "pressed":
         print("Button Is Pressed")
         button_state = None
+    # Code for the Coder to Code the Encoder
+    menu_index = enc.position
+    if last_index is None or menu_index != last_index:
+        print(menu_index)
+    last_index = menu_index
