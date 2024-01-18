b8fff9da72f89d968e455b4287c08fb6e906085a
jmoran40 <jmoran40@charlottesvilleschools.org>
2024-01-18 15:17:46 -0500

    save

--- Trials and Tests/Rotary Encoder & LCD.py
+++ Trials and Tests/Rotary Encoder & LCD.py
@@ -12,3 +12,14 @@ lcd = LCD(I2CPCF8574Interface(board.I2C(), 0X3F), num_rows = 2, num_colms = 16)
 led = neopixel.NeoPixel(board.NEOPIXEL, 1)
 led.brightness = 0.3
 led[0] = (255, 0, 0)
+button = digitalio.DigitalInOut(board.D2)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+button_state = None
+
+while True:
+    # This code registers when the button is pressed
+    if not button.value and button_state is None:
+        button_state = "pressed"
+    if button.value and button_state == "pressed":
+        button_state = None
